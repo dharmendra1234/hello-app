3f39841df2fc825fa9ec71db01378e727da3bf6a
Dharmendra Pawar <dharmendra.pawar@gmail.com>
2020-12-25 17:52:48 -0500

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { TestComponent } from './test/test.component';
 import { Test1Component } from './test1/test1.component';
 import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
+import { CustomerComponent } from './customer/customer.component';
+import { StudentComponent } from './student/student.component';
+import { SchoolComponent } from './school/school.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    CustomerComponent,
+    StudentComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule,
